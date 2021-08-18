e0ee74a6c136fbb18287017b08f68b67d7caea45
Zak Warsame <zakwarsamee@gmail.com>
2021-08-18 21:41:20 +0000

    countLetter.js implementation

--- countLetters.js
+++ countLetters.js
@@ -7,11 +7,35 @@ const assertEqual = function (actual, expected) {
 };
 
 const countLetters = function (theSentence) {
+  const results = {};
+  let sentenceArr = theSentence.split(" ").join("").split("");
 
-    
+  for (const letter of sentenceArr) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
 };
 
+// TEST CASES
 
+let sentence = "lighthouse in the house";
+let resultingObject = {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+};
 
+let expected = countLetters(sentence);
 
-assertEqual();
+assertEqual(expected["l"], resultingObject["l"]);
