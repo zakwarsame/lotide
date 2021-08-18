8a49be3010d4ce1dcdca08159a78f2e887a3ca15
Zak Warsame <zakwarsamee@gmail.com>
2021-08-18 22:02:49 +0000

    letterpositions implementation

---
+++ letterPositions.js
@@ -0,0 +1,54 @@
+const eqArrays = function (arrOne, arrTwo){
+    if (arrOne.length !== arrTwo.length){
+        return false
+    } else {
+        
+        for (i=0;i<arrOne.length;i++){
+        if (arrOne[i]===arrTwo[i]){
+            return true
+        }
+        else{
+            return false
+        }
+        }
+    }
+}
+
+const assertArraysEqual = function (arr1, arr2){
+    if (eqArrays(arr1, arr2)) {
+        console.log(`✅️✅️✅️Assertion Passed: [${arr1}] === [${arr2}]`);
+      } else {
+        console.log(`🛑🛑🛑Assertion Failed: [${arr1}] !== [${arr2}]`);
+      }
+}
+
+const letterPositions = function (sentence) {
+  const results = {};
+  let sentenceArr = sentence.split("");
+   
+  for (let i = 0; i < sentence.length; i++) {
+    
+      
+    if (results[sentence[i]] ) {
+        results[sentence[i]].push(i);
+        
+      } else {
+        results[sentence[i]]=[i];
+      }
+    console.log(sentence[i], i);
+  }
+  delete results[' ']
+  console.log(results)
+  return results;
+};
+
+assertArraysEqual(letterPositions('lighthouse in the house').l, [0]);
+assertArraysEqual(letterPositions('lighthouse in the house').i, [1, 11]);
+assertArraysEqual(letterPositions('lighthouse in the house').g, [2]);
+assertArraysEqual(letterPositions('lighthouse in the house').h, [3, 5, 15, 18]);
+assertArraysEqual(letterPositions('lighthouse in the house').t, [4, 14]);
+assertArraysEqual(letterPositions('lighthouse in the house').o, [6, 19]);
+assertArraysEqual(letterPositions('lighthouse in the house').u, [7, 20]);
+assertArraysEqual(letterPositions('lighthouse in the house').s, [8, 21]);
+assertArraysEqual(letterPositions('lighthouse in the house').e, [9, 16, 22]);
+assertArraysEqual(letterPositions('lighthouse in the house').n, [12]);
