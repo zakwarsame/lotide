84c165709611a3b49ca48f32910d1ccaba5dbe5b
Zak Warsame <zakwarsamee@gmail.com>
2021-08-18 12:03:03 -0400

    create and implement middle.js

---
+++ middle.js
@@ -0,0 +1,46 @@
+const assertArraysEqual = function (arr1, arr2){
+    if (eqArrays(arr1, arr2)) {
+        console.log(`✅️✅️✅️Assertion Passed: [${arr1}] === [${arr2}]`);
+      } else {
+        console.log(`🛑🛑🛑Assertion Failed: [${arr1}] !== [${arr2}]`);
+      }
+}
+
+
+const eqArrays = function (arrOne, arrTwo){
+    if (arrOne.length !== arrTwo.length){
+        return false
+    } else {
+        
+        for (i=0;i<arrOne.length;i++){
+        if (arrOne[i]===arrTwo[i]){
+            return true
+        }
+        else{
+            return false
+        }
+        }
+    }
+}
+
+const middle = function (arr){
+    let middleArr = []
+    if (arr.length <= 2){
+        return 'array elements should be more than 2';
+    }
+    else if (arr.length % 2 !== 0){
+       let index = (parseInt(arr.length/2))
+       middleArr.push(arr[index])
+       return middleArr
+    }
+    else if (arr.length % 2 === 0){
+        let index1 = (parseInt((arr.length/2)-1))
+        let index2 = (parseInt(arr.length/2))
+        middleArr.push(arr[index1])
+        middleArr.push(arr[index2])
+        return middleArr
+    }
+
+}
+
+console.log(middle([1, 2, 3, 5, 7, 8,7 , 9, 4, 18]))// => []
