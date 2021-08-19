ea33563e8b3394f6ba7398babd598838d9b6b17d
Zak Warsame <zakwarsamee@gmail.com>
2021-08-19 00:40:04 +0000

    implement asserobjectequal.js

--- eqObjects.js
+++ eqObjects.js
@@ -20,8 +20,6 @@ const eqArrays = function (arrOne, arrTwo) {
   }
 };
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
   let obj1 = Object.keys(object1);
   let obj2 = Object.keys(object2);
