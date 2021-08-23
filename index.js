788f41d1e4e0cd52c891fa517255ebaca8e8283e
Zak Warsame <zakwarsamee@gmail.com>
2021-08-23 21:12:49 +0000

    implement index.js to help with exporting all functions

---
+++ index.js
@@ -0,0 +1,16 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArraysEqual,
+  assertEqual,
+};
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,12 @@
-
-const assert = require('chai').assert;
-const middle   = require('../middle');
+const assert = require("chai").assert;
+const middle = require("../middle");
 
 describe("#middle", () => {
   it("returns [3] for [1,2,3,4,5]", () => {
-    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
 
   it("returns 'array elements should be more than 2' for [0, 1]", () => {
-    assert.deepEqual(middle([0,1]), 'array elements should be more than 2'); 
+    assert.deepEqual(middle([0, 1]), "array elements should be more than 2");
   });
-
 });
