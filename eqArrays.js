c91dbef98d28042b1e4a65ff5eca4886b92a474d
Zak Warsame <zakwarsamee@gmail.com>
2021-08-23 20:02:17 +0000

    refactored by creating test for eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅️✅️✅️Assertion Passed: [${actual}] === [${expected}]`);
-    } else {
-      console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
-    }
-  };
+const assertEqual = require ('./assertEqual')
 
 
 const eqArrays = function (arrOne, arrTwo){
@@ -25,4 +19,4 @@ const eqArrays = function (arrOne, arrTwo){
 
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+module.exports = eqArrays
